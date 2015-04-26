82d4c8f8a856c8e644aa5d878046c0aab0e9ee2f
spencersmb <spencer.bigum@gmail.com>
2015-04-26 13:00:50 -0400

    eod update blog innner done

--- converted-html/assets/scripts/nav-settings.js
+++ converted-html/assets/scripts/nav-settings.js
@@ -1,7 +1,9 @@
 $(function() {
 
+	//Open close nav
 	var $menu_trigger = $('#sv__nav--trigger'),
 	    $content_wrapper = $('.sv__main--content'),
+	    $footer = $('footer'),
 	    $navigation = $('header');
 
 	    //Open Close Menu
@@ -14,11 +16,46 @@ $(function() {
 
 	        //Slide nav and header over at same time
 	        $navigation.toggleClass('nav-is-open');
+	        $footer.toggleClass('nav-is-open');
 	        $content_wrapper.toggleClass('nav-is-open');
 
 	        //show nav
 	        $('#sv__navigation').toggleClass('nav-is-open');
 
 	    });
+
+    //ON SCROLL Add shadow
+
+    //Get current position of Nav element
+    var currentPosition = $(window).scrollTop();
+
+	if(currentPosition > 50){
+		$navigation.addClass('shadow-on');
+	}else{
+		if($navigation.hasClass('shadow-on')){
+			$navigation.removeClass('shadow-on');
+		}
+	}
+
+    function addShadow(){
+
+    	currentPosition = $(window).scrollTop();
+
+    	if(currentPosition > 50){
+    		$navigation.addClass('shadow-on');
+    	}else{
+    		if($navigation.hasClass('shadow-on')){
+    			$navigation.removeClass('shadow-on');
+    		}
+    	}
+
+
+    }
+
+    $(window).on('scroll', function(){
+    	(!window.requestAnimationFrame) ? addShadow() : window.requestAnimationFrame(addShadow);
+    });
+
+
     
 });
